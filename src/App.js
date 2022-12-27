31a2926f09a20f466a45c33795445717bcb34fa8
vessellook <vessellook@gmail.com>
2022-12-27 18:10:40 +0300

    Add HOC component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 function YearTable(props) {
   console.log('YearTable', props);
@@ -66,6 +67,80 @@ function MonthTable(props) {
   );
 }
 
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name | 'Component';
+}
+
+function withListAdaptation(WrappedComponent, adaptFn, propName = 'list') {
+  const WithListAdaptation = (props) => {
+    const { [propName]: list } = props;
+    const preparedList = adaptFn(list);
+    props = { ...props, [propName]: preparedList };
+    return <WrappedComponent {...props} />;
+  };
+
+  const displayName = getDisplayName(WrappedComponent);
+  const functionName = adaptFn.name || 'function';
+  WithListAdaptation.displayName = `WithListAdaptation(${displayName}, ${functionName}, ${propName})`;
+  return WithListAdaptation;
+}
+
+function sortByDate(list) {
+  const clonedList = structuredClone(list);
+  const compareFn = (a, b) => (a.date < b.date ? -1 : a.date > b.date ? 1 : 0);
+  clonedList.sort(compareFn);
+  return clonedList;
+}
+
+function groupByYear(list) {
+  const yearToTotalAmount = {};
+  list.forEach(({ date, amount }) => {
+    const dateObj = new Date(date);
+    const year = dateObj.getFullYear();
+    if (yearToTotalAmount[year] == null) {
+      yearToTotalAmount[year] = amount;
+    } else {
+      yearToTotalAmount[year] += amount;
+    }
+  });
+  return Object.entries(yearToTotalAmount).map(([year, amount]) => ({
+    year,
+    amount,
+  }));
+}
+
+const groupByMonth =
+  (currentYear = null) =>
+  (list) => {
+    const monthToTotalAmount = {};
+    list.forEach(({ date, amount }) => {
+      const dateObj = new Date(date);
+      if (currentYear != null && currentYear !== dateObj.getFullYear()) {
+        return;
+      }
+      const month = dateObj.toLocaleString('en', { month: 'short' });
+
+      if (monthToTotalAmount[month] == null) {
+        monthToTotalAmount[month] = amount;
+      } else {
+        monthToTotalAmount[month] += amount;
+      }
+    });
+    return Object.entries(monthToTotalAmount).map(([month, amount]) => ({
+      month,
+      amount,
+    }));
+  };
+
+// const now = new Date();
+const currentYear = 2017;
+
+MonthTable = withListAdaptation(MonthTable, groupByMonth(currentYear));
+YearTable = withListAdaptation(YearTable, groupByYear);
+SortTable = withListAdaptation(SortTable, sortByDate);
+
+const DATA_URL = process.env.REACT_APP_DATA_URL;
+
 // TODO:
 // 1. Загрузите данные с помощью fetch: https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json
 // 2. Не забудьте вынести URL в переменные окружения (не хардкодьте их здесь)
@@ -74,6 +149,14 @@ export default class App extends React.Component {
   state = {
     list: [],
   };
+
+  fetchData() {
+    return fetch(DATA_URL).then((response) => response.json());
+  }
+
+  componentDidMount() {
+    this.fetchData().then(({ list }) => this.setState({ list }));
+  }
 
   render() {
     const { list } = this.state;
